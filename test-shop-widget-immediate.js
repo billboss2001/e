7e805227538d592fa4a9756cb8b70b3b5d6bd20c
billboss2001 <35369576+billboss2001@users.noreply.github.com>
2018-11-02 22:38:54 +0100

    Create test-shop-widget-immediate.js

---
+++ test-shop-widget-immediate.js
@@ -0,0 +1,4 @@
+ var scr = document.createElement('script');
+  scr.src = 'https://rawcdn.githack.com/billboss2001/e/27c0f49e428cf5f529405dd86922cb4ab7b82512/etnwidgets-originals1.js';
+  document.body.appendChild(scr);
+  alert(Shopify.checkout.currency)
