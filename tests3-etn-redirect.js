77728c3642debb2478cc76645826bbe88e57e31b
billboss2001 <35369576+billboss2001@users.noreply.github.com>
2018-11-02 22:14:01 +0100

    Update and rename tests2-etn-redirect.js to tests3-etn-redirect.js

--- tests3-etn-redirect.js
+++ tests3-etn-redirect.js
@@ -2,6 +2,9 @@ window.onload=function what(){
 var scr=document.createElement('script');
 scr.src='https://rawcdn.githack.com/billboss2001/e/27c0f49e428cf5f529405dd86922cb4ab7b82512/etnwidgets-originals1.js';
 document.body.appendChild(scr);
+var vendoraddress = 'etn-it-0abc123def456'
+var paymentID = '5b4dc07ce2'
+var amount = '7.89'
 var testdiv = document.getElementById("etnwidget");
-  testdiv.innerHTML = "<div data-etn-vendor='etn-it-0abc123def456/5b4dc07ce2/7.89' data-etn-lang='en'></div>"
+  testdiv.innerHTML = "<div data-etn-vendor='"+vendoraddress+"/"+paymentID+"/"+amount+"' data-etn-lang='en'"></div>"
 }
