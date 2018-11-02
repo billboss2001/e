fc7cf6cdd26d0d91fc94e5375fb6774dc9f99829
billboss2001 <35369576+billboss2001@users.noreply.github.com>
2018-11-02 22:14:38 +0100

    Update tests3-etn-redirect.js

--- tests3-etn-redirect.js
+++ tests3-etn-redirect.js
@@ -6,5 +6,5 @@ var vendoraddress = 'etn-it-0abc123def456'
 var paymentID = '5b4dc07ce2'
 var amount = '7.89'
 var testdiv = document.getElementById("etnwidget");
-  testdiv.innerHTML = "<div data-etn-vendor='"+vendoraddress+"/"+paymentID+"/"+amount+"' data-etn-lang='en'"></div>"
+  testdiv.innerHTML = "<div data-etn-vendor='"+vendoraddress+"/"+paymentID+"/"+amount+"' data-etn-lang='en'></div>"
 }
