02ea1b49a7211df9c7f737ac6f4757ed57cdf84d
billboss2001 <35369576+billboss2001@users.noreply.github.com>
2018-11-02 21:14:42 +0100

    Delete etnwidgets-layer1.js

--- etnwidgets-layer1.js
+++
@@ -1,4 +0,0 @@
-window.onload = function what(){
-var container = document.getElementById("etnwidget");
-container.innerHTML = "<script src='https://rawcdn.githack.com/billboss2001/e/84f6c2f5255a4bdf058e8de6526c5f0171c8548c/widget-original.js'></script>";
-};
