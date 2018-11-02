6628633a5075da1a81e31e5a9228a074081c112a
billboss2001 <35369576+billboss2001@users.noreply.github.com>
2018-11-02 17:51:17 +0100

    Update and rename etnwidget-layer1.js to etnwidgets-layer1.js

--- etnwidgets-layer1.js
+++ etnwidgets-layer1.js
@@ -1,2 +1,4 @@
+window.onload = function what(){
 var container = document.getElementById("etnwidget");
 container.innerHTML = "<script src='https://rawcdn.githack.com/billboss2001/e/84f6c2f5255a4bdf058e8de6526c5f0171c8548c/widget-original.js'></script>";
+};
