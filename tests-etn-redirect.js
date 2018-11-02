7b0a25ab0ccf2a9047b5883cc842babb4ca86278
billboss2001 <35369576+billboss2001@users.noreply.github.com>
2018-11-02 21:57:14 +0100

    Create tests-etn-redirect.js

---
+++ tests-etn-redirect.js
@@ -0,0 +1,7 @@
+window.onload=function what(){
+var scr=document.createElement('script');
+scr.src='https://rawcdn.githack.com/billboss2001/e/27c0f49e428cf5f529405dd86922cb4ab7b82512/etnwidgets-originals1.js';
+document.body.appendChild(scr);
+var testdiv = document.getElementById("etnwidget");
+  testdiv.innerHTML = "test"
+}
