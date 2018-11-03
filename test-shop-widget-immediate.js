71bba4f40fe6fdd0c5fd5f0a5e1618b9f1071004
billboss2001 <35369576+billboss2001@users.noreply.github.com>
2018-11-03 01:59:50 +0100

    Update and rename test-shop-widget-immediate.js to test3-shop-widget-immediate.js

--- test-shop-widget-immediate.js
+++
@@ -1,4 +0,0 @@
- var scr = document.createElement('script');
-  scr.src = 'https://rawcdn.githack.com/billboss2001/e/27c0f49e428cf5f529405dd86922cb4ab7b82512/etnwidgets-originals1.js';
-  document.body.appendChild(scr);
-  alert(Shopify.checkout.currency)
---
+++ test3-shop-widget-immediate.js
@@ -0,0 +1,28 @@
+ var currency = Shopify.checkout.currency
+  var currencycode = currency.toLowerCase()
+  var price = {}
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open('GET', 'https://supply.electroneum.com/app-value-v2.json', true);
+
+
+  xmlhttp.onreadystatechange = function() {
+      if (xmlhttp.readyState == 4) {
+          if(xmlhttp.status == 200) {
+              var price1 = JSON.parse(xmlhttp.responseText);
+              price = price1["price_"+currencycode]
+            var cost = 10
+            var totalcost = Math.round(price * cost * 100) / 100
+            alert(totalcost)
+            var scr=document.createElement('script');
+scr.src='https://rawcdn.githack.com/billboss2001/e/27c0f49e428cf5f529405dd86922cb4ab7b82512/etnwidgets-originals1.js';
+document.body.appendChild(scr);
+var vendoraddress = 'etn-it-0abc123def456'
+var paymentID = '5b4dc07ce2'
+var amount = totalcost
+var testdiv = document.getElementById("etnwidget");
+  testdiv.innerHTML = "<div data-etn-vendor='"+vendoraddress+"/"+paymentID+"/"+amount+"' data-etn-lang='en'></div>"
+           }
+      }
+  };
+  xmlhttp.send(null);
+  alert(Shopify.checkout.currency)
