c6999051c3168fc69e894325446893a5bceb3b28
billboss2001 <35369576+billboss2001@users.noreply.github.com>
2018-11-03 01:15:17 +0100

    Update and rename tests15-etn-redirect.js to tests16-etn-redirect.js

--- tests16-etn-redirect.js
+++ tests16-etn-redirect.js
@@ -12,12 +12,7 @@ window.onload=function what(){
               var price1 = JSON.parse(xmlhttp.responseText);
               price = price1["price_"+currencycode]
             alert(price)
-           }
-      }
-  };
-  xmlhttp.send(null);
-  
-var scr=document.createElement('script');
+            var scr=document.createElement('script');
 scr.src='https://rawcdn.githack.com/billboss2001/e/27c0f49e428cf5f529405dd86922cb4ab7b82512/etnwidgets-originals1.js';
 document.body.appendChild(scr);
 var vendoraddress = 'etn-it-0abc123def456'
@@ -25,5 +20,10 @@ var paymentID = '5b4dc07ce2'
 var amount = '7.89'
 var testdiv = document.getElementById("etnwidget");
   testdiv.innerHTML = "<div data-etn-vendor='"+vendoraddress+"/"+paymentID+"/"+amount+"' data-etn-lang='en'></div>"
+           }
+      }
+  };
+  xmlhttp.send(null);
+  
 
 }
