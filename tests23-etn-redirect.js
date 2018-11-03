f19a75300c29074b29a0f379a8e947c1bfdb9ac0
billboss2001 <35369576+billboss2001@users.noreply.github.com>
2018-11-03 01:45:20 +0100

    Update tests23-etn-redirect.js

--- tests23-etn-redirect.js
+++ tests23-etn-redirect.js
@@ -13,7 +13,8 @@ window.onload=function what(){
               price = price1["price_"+currencycode]
             var cost = 10
             var totalcost = price*cost
-            alert(totalcost)
+            var totalcoststring = totalcost+''
+            alert(totalcoststring)
             var scr=document.createElement('script');
 scr.src='https://rawcdn.githack.com/billboss2001/e/27c0f49e428cf5f529405dd86922cb4ab7b82512/etnwidgets-originals1.js';
 document.body.appendChild(scr);
