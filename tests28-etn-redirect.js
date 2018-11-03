764c28b2544ca7646c68fca0bcf14493205afe07
billboss2001 <35369576+billboss2001@users.noreply.github.com>
2018-11-03 01:54:34 +0100

    Update and rename tests27-etn-redirect.js to tests28-etn-redirect.js

--- tests28-etn-redirect.js
+++ tests28-etn-redirect.js
@@ -13,7 +13,7 @@ window.onload=function what(){
               price = price1["price_"+currencycode]
             var cost = 10
             var totalcost = Math.round(price * cost * 100) / 100
-            alert(totalcoststring)
+            alert(totalcost)
             var scr=document.createElement('script');
 scr.src='https://rawcdn.githack.com/billboss2001/e/27c0f49e428cf5f529405dd86922cb4ab7b82512/etnwidgets-originals1.js';
 document.body.appendChild(scr);
