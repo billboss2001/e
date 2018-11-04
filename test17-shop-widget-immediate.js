f93d2f7e7aafee8bb5041288ef646085182a4696
billboss2001 <35369576+billboss2001@users.noreply.github.com>
2018-11-04 23:57:54 +0100

    Update and rename test16-shop-widget-immediate.js to test17-shop-widget-immediate.js

--- test17-shop-widget-immediate.js
+++ test17-shop-widget-immediate.js
@@ -1,7 +1,9 @@
 if (document.getElementsByClassName("payment-method-list__item__info")[0].innerHTML.indexOf("Electroneum") != -1 ) {
 var currency = Shopify.checkout.currency
+var paymentID = Shopify.checkout.order_id
   var currencycode = currency.toLowerCase()
   var price = {}
+  var amount
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.open('GET', 'https://supply.electroneum.com/app-value-v2.json', true);
 
@@ -17,10 +19,7 @@ var currency = Shopify.checkout.currency
 scr.src='https://rawcdn.githack.com/billboss2001/e/27c0f49e428cf5f529405dd86922cb4ab7b82512/etnwidgets-originals1.js';
 document.body.appendChild(scr);
 var vendoraddress = document.getElementById("vendorAddress").innerHTML 
-var paymentID = (function co(lor){   return (lor +=
-[0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)])
-&& (lor.length == 10) ?  lor : co(lor); })('');
-var amount = totalcost
+amount = totalcost
 var testdiv = document.getElementById("etnwidget");
   testdiv.innerHTML = "<div data-etn-vendor='"+vendoraddress+"/"+paymentID+"/"+amount+"' data-etn-lang='en'></div>"
            }
@@ -28,25 +27,38 @@ var testdiv = document.getElementById("etnwidget");
   };
   xmlhttp.send(null);
   
-       var xmlhttp2 = new XMLHttpRequest();
-    xmlhttp2.open('GET', 'https://fierce-bayou-44059.herokuapp.com/etnpayments.json', true);
+       var pmt
+  var pmtver
+  var xmlhttp2 = new XMLHttpRequest();
+  function getPaymentVer () {
+xmlhttp2.open('GET', 'https://fierce-bayou-44059.herokuapp.com/etnpayments.json?_=' + new Date().getTime(), true);
 
 
-    xmlhttp2.onreadystatechange = function() {
-      var pmtid = '123456789'
-        if (xmlhttp2.readyState == 4) {
-            if(xmlhttp2.status == 200) {
-                var pmt = JSON.parse(xmlhttp2.responseText);
+xmlhttp2.onreadystatechange = function() {
+   if (xmlhttp2.readyState == 4) {
+       if(xmlhttp2.status == 200) {
+           pmt = JSON.parse(xmlhttp2.responseText);
 
-                for (var i = 0; i < pmt.length; i++){
-  if (pmt[i].payment_id == pmtid){
-    alert(pmt[i].amount)
-    alert('hello')
-  }
+           for (var i = 0; i < pmt.length; i++){
+if (pmt[i].payment_id == paymentID){
+pmtver = pmt[i].amount
+}
 }
 
-             }
         }
-    };
-    xmlhttp2.send(null);
+   }
+};
+xmlhttp2.send(null);
+if (pmtver == undefined) {
+  setTimeout(getPaymentVer, 2000)
+
+} else if (pmtver == amount){
+  swal({
+    title: '<div style = "font-family:verdana;">Payment Complete</div>',
+    html: '<div style = "font-family:verdana;">Your order has been accepted</div>',
+    type: 'success'
+  })
+}
+}
+getPaymentVer()
 }
