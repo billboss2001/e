65502fb828f5b5d2e0fcbb9b3c1bbeab0af106c1
billboss2001 <35369576+billboss2001@users.noreply.github.com>
2018-12-11 10:41:59 +0100

    Update status.js

--- status.js
+++ status.js
@@ -3,9 +3,12 @@ xmlhttp4.open('GET', 'https://fierce-bayou-44059.herokuapp.com/appstatus?_=' + n
 xmlhttp4.onreadystatechange = function() {
     if (xmlhttp4.readyState == 4) {
         if(xmlhttp4.status == 503) {
+            console.log("successful execution: " + xmlhttp4.status)
           var div = document.getElementById('etnwidget')
           div.textContent = 'Oops! It seems the ETN Instant Payment system is currently unavailable... Please try again later.'
           div.style.cssText = 'background: rgba(255,0,0,0.2);'
+        } else {
+            console.log("js failed: " + xmlhttp4.status)
         }
       }
     }
