3fa5710134e7258e8ba9564fd9956fab1e9c7ca1
billboss2001 <35369576+billboss2001@users.noreply.github.com>
2018-12-11 11:41:53 +0100

    Update and rename status-3.js to status-4.js

--- status-4.js
+++ status-4.js
@@ -4,7 +4,7 @@ var xmlhttp4 = new XMLHttpRequest();
 xmlhttp4.open('GET', 'https://fierce-bayou-44059.herokuapp.com/appstatus?_=' + new Date().getTime(), true);
 xmlhttp4.onreadystatechange = function() {
     if (xmlhttp4.readyState == 4) {
-        if(xmlhttp4.status == 503) {
+        if(xmlhttp4.status == 0) {
             console.log("successful execution: " + xmlhttp4.status)
           var div = document.getElementById('etnwidget')
           div.textContent = 'Oops! It seems the ETN Instant Payment system is currently unavailable... Please try again later.'
